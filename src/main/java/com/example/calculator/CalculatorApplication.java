bc76158cd65dd46de5e44398003cbc777d9f0a0e
astik1991 <52450809+astik1991@users.noreply.github.com>
2021-05-06 18:37:48 +0530

    Update CalculatorApplication.java

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class CalculatorApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(CalculatorApplication.class); 
 	}
 
 }
